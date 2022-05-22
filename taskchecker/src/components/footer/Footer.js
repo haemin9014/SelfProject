e9b4dda687bf69f8e6454e0b064caf2217f9a4cd
haemin9014 <leehaemin9014@gmail.com>
2022-05-22 13:07:05 +0900

    basic skeleton

---
+++ taskchecker/src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+
+class Footer extends Component {
+  render() {
+    return (
+      <Fragment>
+        <center>
+          <h1>Footer</h1>
+        </center>
+        <hr />
+      </Fragment>
+    );
+  }
+}
