8e51abad50c1d66645a24a48e02fcd494c1f3152
haemin9014 <leehaemin9014@gmail.com>
2022-05-22 11:33:17 +0900

    cleaning up and building skeleton

---
+++ taskchecker/src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <hr />
+      <center>
+        <h1>Header</h1>
+      </center>
+    </div>
+  );
+};
+
+export default Header;
